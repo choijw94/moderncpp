133d6bc7030e0181f792224476a48f5ddbaf7a50
ecourse <smkang@codenuri.co.kr>
2023-12-11 14:50:57 +0900

    Add files via upload

--- DAY1/14_override2.cpp
+++ DAY1/14_override2.cpp
@@ -4,7 +4,7 @@ template<typename T>
 class Base
 {
 public:
-	virtual void foo(const T p)
+	virtual void foo(const T a)
 	{
 		std::cout << "Base foo" << std::endl;
 	}
@@ -15,8 +15,9 @@ class Derived : public Base<int*>
 public:
 	// foo 재정의하세요 "Derived foo" 출력, override 키워드 사용하지 마세요
 };
+
 int main()
 {
 	Base<int*>* p = new Derived;
-	p->foo(0);
+	p->foo(0);	// "Derived foo"
 }
