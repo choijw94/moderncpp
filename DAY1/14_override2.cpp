3d664a0cc2ae11a135a36d7749af734fa624a40d
ecourse <smkang@codenuri.co.kr>
2023-12-11 14:58:30 +0900

    Add files via upload

--- DAY1/14_override2.cpp
+++ DAY1/14_override2.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+int n = 0;
+
+// #1. 아래 2개 차이점 명확히 알아야 합니다.
+const int* p1 = &n; // p1을 따라가면 const, p1 자체는 상수 아님.
+int* const p2 = &n; // p2가 const,         p2가 가리키는 곳은 const 아님.
+
+// #2. const 타입의 앞 또는 뒤에 표시해도 됩니다.
+const int c1 = 10;
+int const c2 = 10; // c1, c2 는 동일
+
+const int* p1 = &n;
+int const* p2 = &n;  // p1, p2 는 동일
+int* const p3 = &n;  // p3는 p1, p2 와 다름. * 위치 에 주의
+
+
+
+
 template<typename T>
 class Base
 {
@@ -14,6 +31,9 @@ class Derived : public Base<int*>
 {
 public:
 	// foo 재정의하세요 "Derived foo" 출력, override 키워드 사용하지 마세요
+	// => 아래 재정의 코드는 잘못된 재정의 입니다.
+	// => "override" 키워드를 사용했다면 "잘못되었다는 사실을 바로 알아낼수 있습니다."
+	// => "override" 키워드를 붙이지 않으면, 잘못되었다는 것을 바로 알수 없습니다.
 	virtual void foo(const int* a)
 	{
 		std::cout << "Derived foo" << std::endl;
