0a2b366a4aeb2d9c5e27f81dd13ef8860dfa55ce
ecourse <smkang@codenuri.co.kr>
2023-12-11 16:35:46 +0900

    Add files via upload

--- DAY1/18_if_init.cpp
+++ DAY1/18_if_init.cpp
@@ -11,8 +11,30 @@ int main()
 	}
 
 	// C++17 부터 if 문을 아래 처럼 사용가능합니다.
+	// if ( 초기화코드; 조건식 )
 	if (int ret = foo(); ret == 0)
 	{
 
+	}	// <== ret 파괴
+
+	
+	// switch 도 가능합니다.
+	switch (int n = foo(); n)
+	{
+	case 1: break;
+	case 2: break;
 	}
+
+	// while 문도 될까요 ?
+	// => 안됩니다.
+	// => 만들 필요가 없었습니다. (for 문 사용하면 되므로)
+	/*
+	while (int cnt = 0; cnt < 10)
+	{
+		++cnt;
+	}
+	for (int cnt = 0; cnt < 10; ++cnt)
+	{
+	}
+	*/
 }
