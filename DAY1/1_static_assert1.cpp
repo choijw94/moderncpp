75d0b729dfd0d8faf3f072974cdcac28672fcf17
ecourse <smkang@codenuri.co.kr>
2023-12-11 09:35:20 +0900

    Add files via upload

---
+++ DAY1/1_static_assert1.cpp
@@ -0,0 +1,24 @@
+#include <cassert>
+
+// assert        : 실행시간 표현식의 유효성을 확인하는 도구, C/C++
+// static_assert : 컴파일 시간에 유효성을 확인.
+//					표현식이 거짓이면 컴파일 멈추고 메세지 출력.
+
+static_assert(sizeof(int*) >= 8, "error, not 64bit");
+
+
+void foo(int age)
+{
+	// 함수 인자를 사용하기 전에 유효성을 확인하는 것이 좋습니다.
+	assert(age > 0);
+
+	int* p = new int[age];
+
+	delete[] p;
+}
+
+int main()
+{
+//	foo(10);
+	foo(-10); // 실수했다.!!
+}
