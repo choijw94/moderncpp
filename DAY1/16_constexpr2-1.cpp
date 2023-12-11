3794b7800119735b07e4be9a808b18ce5ad8ab5a
ecourse <smkang@codenuri.co.kr>
2023-12-11 15:49:59 +0900

    Add files via upload

--- DAY1/16_constexpr2-1.cpp
+++ DAY1/16_constexpr2-1.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
-constexpr int add(int a, int b)
+// std::is_constant_evaluated()
+// => 컴파일 시간에 실행되었는지 확인 할때 사용
+// => C++20
+
+constexpr std::pair<int, bool> add(int a, int b)
 {
 	int c = a + b;
-	return c;
+
+	bool ret = std::is_constant_evaluated();
+
+	return std::pair<int, bool>(c, ret);
 }
 
 int main()
 {
-	add(1, 2);
+	auto           ret1 = add(1, 2);
+	constexpr auto ret2 = add(1, 2); // 컴파일 시간!!
+
+	std::cout << ret1.second << std::endl;
+	std::cout << ret2.second << std::endl;
 }
