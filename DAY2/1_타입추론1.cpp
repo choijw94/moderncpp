58230c587de8087cea63aaff1e427b8e2c4c6b04
ecourse <smkang@codenuri.co.kr>
2023-12-12 08:52:32 +0900

    Add files via upload

--- DAY2/1_타입추론1.cpp
+++ DAY2/1_타입추론1.cpp
@@ -4,14 +4,31 @@
 template<typename T> void foo(T a)
 {
 	// #1. RTTI 기술 사용
-	std::cout << typeid(T).name() << std::endl;
+	// => T 가 가진 const, volatile, reference 를 제거한 결과만 나오게 됩니다.
+	// => 절대 이렇게 조사하지 마세요
+	// => 현재 C++ 의 RTTI 기술이 완전하지 않습니다. C++26 에서 새로운 기술 등장
+	// std::cout << typeid(T).name() << std::endl;
+
+
+	// #2. 컴파일러에 의해 생성된 함수이름 출력
+	// __func__            : C++ 표준 매크로, 함수이름만 담고 있다. 인자는 포함안됨. 
+	// __FUNCSIG__         : 인자와 반환 타입을 포함한 함수이름. C++ 표준 아님. VS 만 지원
+	// __PRETTY_FUNCTION__ : 인자와 반환 타입을 포함한 함수이름. C++ 표준 아님. g++ 만 지원
+	
+	//std::cout << __func__ << std::endl;
+	std::cout << __FUNCSIG__ << std::endl;
+
+
 }
+
+
 
 int main()
 {
 	// #1. 타입인자를 명시적으로 전달하는 경우
 	// => 사용자가 전달한 타입 사용
-	foo<int>(3.4); // T = int
+//	foo<int>(3.4);      // T = int
+	foo<const int&>(3); // T = const int&
 
 	
 	// #2. 타입인자를 전달하지 않은 경우. 
