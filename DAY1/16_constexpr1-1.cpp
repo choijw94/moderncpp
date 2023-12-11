cb810689f369e8a2b35f496389c84bbf2afec014
ecourse <smkang@codenuri.co.kr>
2023-12-11 15:32:33 +0900

    Add files via upload

---
+++ DAY1/16_constexpr1-1.cpp
@@ -0,0 +1,15 @@
+// 16_constexpr1-1
+
+int main()
+{
+	int n = 10;
+
+	// const : read only(immutable)을 만드는 것
+	const int c1 = 10; // ok.  literal 로 초기화 해도되고
+	const int c2 = n;  // ok.  변수로 초기화도 가능
+
+
+	// C++11 constexpr : 컴파일 시간에 값을 알수있는 상수을 만드는 것
+	constexpr int c3 = 10; // ok
+	constexpr int c4 = n;  // error.
+}
