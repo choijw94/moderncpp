7679fa8e870b1403f67ae2a7cf706a09c240f396
ecourse <smkang@codenuri.co.kr>
2023-12-11 13:38:19 +0900

    Add files via upload

---
+++ DAY1/9_enum2.cpp
@@ -0,0 +1,21 @@
+
+// C++98 enum
+// enum COLOR { red = 1, green = 2 };
+
+// C++11 enum
+enum class COLOR { red = 1, green = 2 };
+
+int main()
+{
+//	int c1 = red;	      // error. COLOR 생략할수 없음.
+//	int c1 = COLOR::red;  // error. int 로 암시적 변환 안됨
+
+	COLOR c1 = COLOR::red;  // ok. 이렇게만 사용가능.
+							// 보다 엄격한 규칙대로만 사용가능
+							// 버그를 줄일수 있다.
+}
+
+
+
+
+
