43fa936a3a1f2032e96ed0994af5aa25b2580038
ecourse <smkang@codenuri.co.kr>
2023-12-11 17:16:45 +0900

    Add files via upload

--- DAY1/string_view3.cpp
+++ DAY1/string_view3.cpp
@@ -8,7 +8,6 @@ void* operator new(std::size_t size)
 
 	return malloc(size);
 }
-
 void f2(const std::string& s) {}
 
 void f3(std::string_view sv) {}
@@ -16,9 +15,12 @@ void f3(std::string_view sv) {}
 
 int main()
 {
-	f2("to be or not to be");	// 임시객체를 생성하고, 문자열을 힙에 복사하기 위해
+//	f2("to be or not to be");	// 임시객체를 생성하고, 문자열을 힙에 복사하기 위해
 								// new 를 사용하게 됩니다.
 				
 
-//	f3("to be or not to be");	// 임시객체 같은것이 생성되지 않음. new 사용안함.
+	f3("to be or not to be");	// 임시객체 같은것이 생성되지 않음. new 사용안함.
 }
+// RUST     C++
+// String   std::string
+// str      std::string_view
