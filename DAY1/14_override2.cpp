6945a2e71e6aac8b6010021361e44326dfc5141f
ecourse <smkang@codenuri.co.kr>
2023-12-11 14:52:56 +0900

    Add files via upload

--- DAY1/14_override2.cpp
+++ DAY1/14_override2.cpp
@@ -14,10 +14,14 @@ class Derived : public Base<int*>
 {
 public:
 	// foo 재정의하세요 "Derived foo" 출력, override 키워드 사용하지 마세요
+	virtual void foo(const int* a)
+	{
+		std::cout << "Derived foo" << std::endl;
+	}
 };
 
 int main()
 {
 	Base<int*>* p = new Derived;
-	p->foo(0);	// "Derived foo"
+	p->foo(0);	// "Derived foo" 나오도록 Derived 안에 foo 만드세요
 }
