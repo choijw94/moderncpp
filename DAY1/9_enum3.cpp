c17c490c7e605045ff1a5956ed1ba40844b5a527
ecourse <smkang@codenuri.co.kr>
2023-12-11 13:41:55 +0900

    Add files via upload

---
+++ DAY1/9_enum3.cpp
@@ -0,0 +1,14 @@
+
+// 각 요소들의 내부 표현은 int 사용합니다.
+// enum class DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+
+// 각 요소의 내부 표현을 char 로 사용하라는 의미.
+enum class DAYOFWEEK : char { SUN = 0, MON = 1, TUE = 2 };
+
+int main()
+{
+}
+
+
+
+
