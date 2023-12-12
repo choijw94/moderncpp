c5f22f356e5f3eff91c2e423b95d60337b5d179d
ecourse <smkang@codenuri.co.kr>
2023-12-12 15:58:37 +0900

    Add files via upload

--- DAY2/5_rvalue3.cpp
+++ DAY2/5_rvalue3.cpp
@@ -6,4 +6,16 @@ int main()
 	int n = 10;
 
 	n + 5 = 20; // error. "n + 5" 는 rvalue  
+
+
+	// lvalue, ravlue 를 조사하려면
+	// => decltype 의 규칙을 잘생각해 보세요
+	using T = decltype(n + 5);	
+				// n+5 가 lvalue 라는 T 는 참조 타입
+				//        rvalue 라는 T 는 값 타입
+
+	if (std::is_lvalue_reference_v<T>)
+		std::cout << "lvalue" << std::endl;
+	else 
+		std::cout << "rvalue" << std::endl;
 }
