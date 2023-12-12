ae8f5d5678d2b6fe8b63d307642305f6ae638b75
ecourse <smkang@codenuri.co.kr>
2023-12-12 11:59:54 +0900

    Add files via upload

--- DAY2/3_초기화5-1.cpp
+++ DAY2/3_초기화5-1.cpp
@@ -9,12 +9,17 @@ void foo(Point pt) {}
 
 Point goo() 
 {
-	Point pt(1, 2);
-	return pt;
+//	Point pt(1, 2);
+//	return pt;
+
+	return { 1,2 };	// C++11 부터 이렇게 반환도 가능합니다.
 }
 
 int main()
 {
 	Point pt(1, 2);
 	foo(pt);
+
+	foo({ 1,2 }); // C++11 부터 이렇게 전달 가능합니다.
+				  // Point pt = {1, 2}
 }
