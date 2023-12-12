e27f7b5fdd62ceecdde708e50bf0de6955588c97
ecourse <smkang@codenuri.co.kr>
2023-12-12 16:10:06 +0900

    Add files via upload

--- DAY2/5_rvalue4.cpp
+++ DAY2/5_rvalue4.cpp
@@ -11,10 +11,18 @@ int main()
 {
 	int n = 10;
 
-	VALUE_CATEGORY(n);
-	VALUE_CATEGORY(n+1);
-	VALUE_CATEGORY(n = 10);
-	VALUE_CATEGORY(++n);
-	VALUE_CATEGORY(n--);
-	VALUE_CATEGORY(10);
+	VALUE_CATEGORY(n);		// l
+	VALUE_CATEGORY(n+1);	// r
+	VALUE_CATEGORY(n = 10);	// l
+	VALUE_CATEGORY(++n);	// l
+	VALUE_CATEGORY(n--);	// r
+	VALUE_CATEGORY(10);		// r
+
+	VALUE_CATEGORY("hello"); // ?? lvalue ? rvalue
+							// 놀랍게도 lvalue 입니다.
+							// 정수, 실수 리터럴 : rvalue	
+							// 문자열 리터럴     : lvalue 
+
+	"hello"[0] = 'x';	// error. rvalue 라서 에러가 아니라
+						// const 라서 에러 입니다.
 }
