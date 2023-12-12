3b6aed5e977c244479db823069593ce1f3f6bf26
ecourse <smkang@codenuri.co.kr>
2023-12-12 09:04:31 +0900

    Add files via upload

--- DAY2/1_타입추론2.cpp
+++ DAY2/1_타입추론2.cpp
@@ -19,4 +19,18 @@ int main()
 	foo(c);		// T = int
 	foo(r);     // T = int
 	foo(cr);    // T = int
+
+	// 주의 : 인자가 가진 const 속성만 제거됩니다.
+	const char* s1 = "hello";	
+
+	foo(s1); // s1 자체는 const 가 아닙니다. s1이 가리키는 곳이 const 
+			 // 인자의 const 만 제거 되므로 
+			 //	T = const char*
+
+
+	const char* const s2 = "hello";
+	foo(s2); // s2 자체도 const, 가리키는 곳도 const
+			 // s2 : const char* const
+			 // T  : const char* 
+
 }
