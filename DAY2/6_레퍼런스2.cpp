0f228248d6b8fb0a0cf1fe9fa6a8ddda96d60b81
ecourse <smkang@codenuri.co.kr>
2023-12-12 16:56:31 +0900

    Add files via upload

--- DAY2/6_레퍼런스2.cpp
+++ DAY2/6_레퍼런스2.cpp
@@ -48,12 +48,21 @@ int main()
 	// f1(int&)  : int&  타입을 받겠다는 것이 아니라 lvalue 을 받겠다는 것
 	// f1(int&&) : int&& 타입을 받겠다는 것이 아니라 rvalue 을 받겠다는 것
 
+
 	// #3.
 	int&& r3 = 10;	// 10 은 rvalue, 하지만 r3 는 lvalue
 					// "named rvalue reference is lvalue" - 구글 검색
 
-	foo(r3);					 // 1번
+	foo(r3);					 // 1번. r3 타입은 int&&, value 속성 lvalue
+
 	foo(static_cast<int&&>(r3)); // 3번
+			// => r3의 타입이 int&& 인데 ???
+			// => 다시 int&& 로 캐스팅???
+			// => 결국 같은 타입 아닌가요 ?? 이게 어떤 의미가 있지요 ??
+			
+	// static_cast<Type>(r3) 에서 Type 이 && 가 붙어 있는 경우
+	// => "타입 캐스팅" 이 아닌 "value 속성을 변경하는 캐스팅" 입니다.
+	// => cppreference.com 에서 "static_cast" 찾아 보세요
 }
 
 
