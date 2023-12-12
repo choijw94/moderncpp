41cfb43b811e7dc1c0b86b9dc6b4814482b4a8da
ecourse <smkang@codenuri.co.kr>
2023-12-12 10:45:58 +0900

    Add files via upload

--- DAY2/평가되지않은표현식1.cpp
+++ DAY2/평가되지않은표현식1.cpp
@@ -15,5 +15,17 @@ int main()
 
 
 	// #3. decltype(exp) : exp 이 생성하는 값의  타입을 결정하는 것
+	// => 이때 exp 는 실제 "평가(실행)되지는 않습니다."
+	// => 평가되지 않은 표현식(unevaluated expression) 이라고 합니다.
 	decltype( add(1, 2) ) ret = add(1, 2);
+
+	// C++에서 평가되지 않은 표현식은 4개가 있습니다.
+	decltype(add(1, 2)) d; 
+	
+	int n = sizeof(add(1, 2));
+
+	bool b = noexcept(add(1, 2));
+
+	const std::type_info& t = typeid(add(1, 2));
+
 }
