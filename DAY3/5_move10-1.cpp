ede5c007cc6d7d90ff8883c9f0c3ce890a2d2a1d
ecourse <smkang@codenuri.co.kr>
2023-12-13 16:59:33 +0900

    Add files via upload

--- DAY3/5_move10-1.cpp
+++ DAY3/5_move10-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+// #1. move 계열에서는 멤버를 std::move 로 옮겨라!
 
 class Object
 {
@@ -8,14 +9,15 @@ class Object
 public:
 	Object(const std::string& n) : name(n) {}
 
-	Object(const Object& obj) : name(obj.name) { std::cout << "copy" << std::endl; }
+	Object(const Object& obj) : name(obj.name) {  }
 
 	Object(Object&& obj) noexcept 
-		: name(obj.name)
+//		: name(obj.name)				// 나쁜 코드 입니다. std::string 의 복사 생성자 호출
+		: name( std::move(obj.name) )	// 좋은 코드. string 의 move 생성자 호출
 	{
-		std::cout << "move" << std::endl;
 	}
 };
+
 
 int main()
 {
