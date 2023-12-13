8ee78bad5207dfbb0bf40892a1dd133f52df07b5
ecourse <smkang@codenuri.co.kr>
2023-12-13 15:07:23 +0900

    Add files via upload

--- DAY3/5_move05-1.cpp
+++ DAY3/5_move05-1.cpp
@@ -8,10 +8,16 @@ public:
 	Object(Object&& obj)      { std::cout << "move\n"; }
 };
 
+template<typename T>
+T&& mymove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+
 int main()
 {
 	Object o1;
-	Object o2 = o1;
-	Object o3 = std::move(o1);
-	Object o4 = mymove(o1);		// 만들어 봅시다.
+	Object o2 = o1;				// copy
+	Object o3 = std::move(o1);	// move
+	Object o4 = mymove(o1);		// move 만들어 봅시다.
 }
