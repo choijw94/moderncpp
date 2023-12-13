2d78a04585d0db1d37e9f712d0379505d342fe35
ecourse <smkang@codenuri.co.kr>
2023-12-13 15:11:22 +0900

    Add files via upload

--- DAY3/5_move05-1.cpp
+++ DAY3/5_move05-1.cpp
@@ -8,8 +8,14 @@ public:
 	Object(Object&& obj)      { std::cout << "move\n"; }
 };
 
+// 템플릿 만들때 인자를 T& 로 할지 T&& 로 할지 잘 판단하세요 
+// T&  : 인자로 lvalue 만 받겠다는 의도
+// T&& : 인자로 lvalue, rvaue 를 모두 받겠다는 의도
+
+// move : 인자로 lvalue, rvalue 를 모두 받아서 rvalue 로 캐스팅
+
 template<typename T>
-T&& mymove(T& obj)
+T&& mymove(T&& obj)
 {
 	return static_cast<T&&>(obj);
 }
@@ -19,5 +25,7 @@ int main()
 	Object o1;
 	Object o2 = o1;				// copy
 	Object o3 = std::move(o1);	// move
-	Object o4 = mymove(o1);		// move 만들어 봅시다.
+	Object o4 = mymove( o2 );		// move 만들어 봅시다. 
+
+	Object o5 = mymove( Object() ); // 되야 할까요 ? 안되야 할까요 ?
 }
