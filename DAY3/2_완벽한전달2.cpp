a977d379040c5f309bfec4856ba307c4a3188d65
ecourse <smkang@codenuri.co.kr>
2023-12-13 09:56:13 +0900

    Add files via upload

--- DAY3/2_완벽한전달2.cpp
+++ DAY3/2_완벽한전달2.cpp
@@ -6,12 +6,25 @@
 void foo(int a) {}
 void goo(int& a) { a = 100; }
 
+// 완벽한 전달을 하려면
+// 1. 인자의 복사본을 만들면 안된다. call by reference 사용!
+// 2. lvalue, rvalue 를 모두 받아야 한다.
+// 3. 상수성이 추가되어도 안된다.
+
+// ==> int&, int&& 버전을 따로 제공해야 한다.
 
 template<typename F>
-void chronometry(F f, int arg)
+void chronometry(F f, int& arg)
 {
 	f(arg);	
 }
+
+template<typename F>
+void chronometry(F f, int&& arg)
+{
+	f(arg);
+}
+
 
 int main()
 {
