233e0cbd72b878f82e4f4771f8599b4e417cd300
ecourse <smkang@codenuri.co.kr>
2023-12-13 11:53:09 +0900

    Add files via upload

--- DAY3/3_완벽한전달활용1.cpp
+++ DAY3/3_완벽한전달활용1.cpp
@@ -49,3 +49,30 @@ int main()
 //				  전달 받은 인자를 어떠한 변경도 없이 생성자에 보내야 합니다
 //				  "완벽한 전달" 기술이 없으면 만들수 없습니다.
 
+// 대략 emplace_back 은 아래 형태가 되게 됩니다.
+/*
+template<typename T> 
+decltype(auto) emplace_back(T&& ... args)
+{
+	new Point(std::forward<T>(args)...);
+}
+*/
+
+// STL 컨테이너 사용시
+// #1. primitive 타입을 저장하는 컨테이너 ( std::vector<int> v) 의 경우
+// => v.push_back(1)    <== 이렇게 하세요 (가독성이 아래 보다 좋습니다.)
+// => v.emplace_back(1) <== 이렇게 해도 됩니다.
+
+
+// #2. user define type 포인터를 저장(std::vector<Point*> v)
+// => v.push_back( new Point ) <== 역시, push_back 사용하세요
+
+// #3. user define type 을 value 로 보관 한다면 (std::vector<Point> v)
+// => v.push_back( Point(1,2))  보다는 
+// => v.emplace_back( 1, 2)     가 훨씬 빠릅니다.  되도록 이렇게 하세요
+
+// 단, 객체를 사용하다거 넣는다면 push_back, emplace_back 동일..
+// Point pt(1,2); // 만들고..
+// pt 를 사용하다가..
+// v.push_back(pt);    // 이렇게 하거나
+// v.emplace_back(pt); // 이렇게 하거나 동일.
