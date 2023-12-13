2a8b0fff7bb5f829511881ca9a9360aa94aa4a47
ecourse <smkang@codenuri.co.kr>
2023-12-13 12:02:17 +0900

    Add files via upload

--- DAY3/3_완벽한전달활용2.cpp
+++ DAY3/3_완벽한전달활용2.cpp
@@ -22,7 +22,15 @@ int main()
 	//    아래 코드는 동적 메모리를 몇번 할당 할까요 ?
 	// => new Point 때문에 1번 호출
 	// => control block 을 만들기 위해서 1번 호출(shared_ptr 생성자에서)
-	std::shared_ptr<Point> sp1( new Point(1,2) );
+//	std::shared_ptr<Point> sp1( new Point(1,2) );
+
+
+	// 2.
+	// => Point 객체와 control block 을 붙여서 한번에 할당하면
+	//    메모리 효율이 좋아 집니다.
+	// => 그렇게 하려면 사용자가 "new Point" 하면 안되고..
+	//    라이브러리 내부에서 "sizeof(Point) + sizeof(control block)" 을 
+	//    한번에 할당해야 합니다.
 }
 
 
