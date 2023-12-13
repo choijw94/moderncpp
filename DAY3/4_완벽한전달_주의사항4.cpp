551a5b54bfa960c6966496e49c5105b66c2b7a85
ecourse <smkang@codenuri.co.kr>
2023-12-13 13:42:43 +0900

    Add files via upload

--- DAY3/4_완벽한전달_주의사항4.cpp
+++ DAY3/4_완벽한전달_주의사항4.cpp
@@ -3,9 +3,14 @@ int main()
 	int n = 3;
 
 	// 다음중 에러를 모두 고르세요!
-	auto& a1 = n;
-	auto& a2 = 3;
+	auto& a1 = n;	// ok
+	auto& a2 = 3;	// error.   3 은 rvalue
 
-	auto&& a3 = n;
-	auto&& a4 = 3;
+	// auto 는 T 와 규칙이 동일합니다.
+	// 따라서 auto&& 는 T&& 입니다
+	// auto&& 는 rvalue reference 아닙니다. forwarding reference
+	// => 따라서 아래 코드는 모두 에러 아닙니다.
+
+	auto&& a3 = n;	// n 은 lvalue, auto(T)=int&, auto&&=int& &&.  int&  a3 = n;
+	auto&& a4 = 3;  // 3 은 rvalue  auto(T)=int   auto&&=int&&     int&& a4 = 3;
 }
