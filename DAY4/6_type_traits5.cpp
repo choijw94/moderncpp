0e066984d4f607d22253a12c1cf0bdb2219e0d88
ecourse <smkang@codenuri.co.kr>
2023-12-14 17:09:18 +0900

    Add files via upload

--- DAY4/6_type_traits5.cpp
+++ DAY4/6_type_traits5.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <type_traits>
+// 모든 포인터를 제거한 타입을 구해 봅시다.
+// => C++ 표준에는 아래 코드는 없습니다.!!! 
+// => C++ 표준에는 remove_pointer 만 있습니다.
 
-template<typename T> struct remove_pointer
+template<typename T> struct remove_all_pointer
 {
 	using type = T;
 };
 
-template<typename T> struct remove_pointer<T*>
+template<typename T> struct remove_all_pointer<T*>
 {
-	using type = T;
+	using type = remove_all_pointer<T>::type ;
 };
 
 int main()
 {
-	remove_pointer<int***>::type n;
+	remove_all_pointer<int***>::type n;
 }
