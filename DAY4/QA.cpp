dd8d34e44ec4cae61f3fad40725f43ef70e3e2ec
ecourse <smkang@codenuri.co.kr>
2023-12-14 14:10:22 +0900

    Add files via upload

---
+++ DAY4/QA.cpp
@@ -0,0 +1,27 @@
+// QA
+// nasm 을 학습하는게 좋습니다.
+// 1. windows 와 linux, osx 모두 가능
+// 2. intel 어셈과 거의 동일, godbolt.org 문법과 거의 동일
+// 3. 문법 쉽고, 자료 많습니다.
+// 4. "nasm tutorial"
+
+// 함수와 클래스(구조체)
+void foo()
+{
+	// ...
+}
+
+class Point
+{
+	int x;
+	int y;
+public:
+	void set(int a, int b)
+	{
+	}
+};
+
+int main()
+{
+	Point pt;
+}
