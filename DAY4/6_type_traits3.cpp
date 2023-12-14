72b332228ab9cfff9bf95a11c11b705cd2e8f28f
ecourse <smkang@codenuri.co.kr>
2023-12-14 16:55:49 +0900

    Add files via upload

--- DAY4/6_type_traits3.cpp
+++ DAY4/6_type_traits3.cpp
@@ -7,7 +7,17 @@
 
 template<typename T> void printv(const T& v)
 {
-	if ( std::is_pointer<T>::value )
+	// if : 실행시간 조건문, 컴파일 시간에 조건이 false 로 결정되어도
+	//      템플릿 => 실제 함수 를 만들때는 포함됩니다.
+	//      단지, 실행만 안되는 것
+
+	// if constexpr : 컴파일 조건문, 컴파일 시간에 조건이 false 로 결정되면
+	//				  템플릿 = > 실제 함수 를 만들때는 포함 안됨.
+	//                C++17
+	// 
+//	if ( std::is_pointer<T>::value )
+
+	if constexpr (std::is_pointer<T>::value)
 
 		std::cout << v << " : " << *v << std::endl;
 
