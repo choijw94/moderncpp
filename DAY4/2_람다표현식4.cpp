2c36745e9e72d0bbcaa5214a50db5583237a39c2
ecourse <smkang@codenuri.co.kr>
2023-12-14 11:42:18 +0900

    Add files via upload

--- DAY4/2_람다표현식4.cpp
+++ DAY4/2_람다표현식4.cpp
@@ -17,6 +17,21 @@ int main()
 	std::cout << typeid(f1).name() << std::endl;
 	std::cout << typeid(f2).name() << std::endl;
 
+
+	// #2. 람다표현식으로 초기화된 변수에, 다른 람다표현식 담을수 없습니다.
+	auto f3 = [](int a, int b) { return a + b; };
+
+//	f3 = [](int a, int b) { return a + b; }; // error. 타입이 다릅니다.
+
+
+	// #3. 
+	// 아래 코드를 잘 생각해 보세요. sort() 함수가 몇개 생성되었을까요 ?
+	std::vector<int> v = { 1,2,3 };
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+
 }
 
 
