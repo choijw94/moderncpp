3f0a8bbdd5ce9b1ee2d63e5cb65054a5a898d37f
ecourse <smkang@codenuri.co.kr>
2023-12-14 16:38:49 +0900

    Add files via upload

--- DAY4/6_type_traits2.cpp
+++ DAY4/6_type_traits2.cpp
@@ -3,16 +3,27 @@
 
 // type traits : 타입(T) 에 대한 다양한 특성(특질)을 조사하는 기술
 
+// 만드는 법 : 대부분 아래와 유사한 원리로 만들게 됩니다.
+// 
+// 1. primary template 에서는 false 반환(value = false 라는 의미)
+// 2. 조건을 만족하도록 부분 특수화 버전을 제공하고 true 반환
+
 template<typename T>
 struct is_pointer
 {
 	enum { value = false };
 };
 
+template<typename T>
+struct is_pointer<T*>
+{
+	enum { value = true };
+};
 
 
 template<typename T> void foo(const T& a)
 {
+	// T : int, int*
 	if ( is_pointer<T>::value )
 		std::cout << "포인터" << std::endl;
 	else
