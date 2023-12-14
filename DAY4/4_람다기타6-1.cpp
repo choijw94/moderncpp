3186002c7deb0d5b44e33741f3ca409e3bbbf344
ecourse <smkang@codenuri.co.kr>
2023-12-14 15:39:50 +0900

    Add files via upload

--- DAY4/4_람다기타6-1.cpp
+++ DAY4/4_람다기타6-1.cpp
@@ -2,5 +2,9 @@
 #include <vector>
 
 // 함수 인자를 읽기만 하려고 합니다. 다음중 좋은 코드는 ?
-void foo(std::vector<int> v) {}
-void foo(const std::vector<int>& v) {}
+void foo(std::vector<int> v) {}			// bad. 절대 값으로 call by value 사용하지마세요
+void foo(const std::vector<int>& v) {}  // good.
+
+// 다음중 좋은 코드는 ?
+void foo(int n)			{ int a = n; } // 1
+void foo(const int& n)	{ int a = n; } // 2
