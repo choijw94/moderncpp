165063a8c4c8b7c91896c43f29007b177d1d2184
ecourse <smkang@codenuri.co.kr>
2023-12-14 17:04:46 +0900

    Add files via upload

--- DAY4/6_type_traits4.cpp
+++ DAY4/6_type_traits4.cpp
@@ -18,16 +18,16 @@ template<typename T> struct remove_pointer<T*>
 	using type = T;
 };
 
-
-
 int main()
 {
 	remove_pointer<int*>::type n2;  // int n2
 }
 
-
-
-
 template<typename T> void foo(T a)
 {
+	// 주의 : 변형 타입을 얻는 traits 는 결국 "값"이 아닌 "타입"을 얻게 됩니다.
+	//		템플릿 의존 이름을 사용한다면 "typename" 필요 합니다.
+
+	remove_pointer<T>::type n1; // error
+	typename remove_pointer<T>::type n1; // ok.
 }
