e6e4792173a09718afc446d42c8b47b20e93741a
ecourse <smkang@codenuri.co.kr>
2023-12-15 11:37:29 +0900

    Add files via upload

--- DAY5/4_가변인자템플릿6.cpp
+++ DAY5/4_가변인자템플릿6.cpp
@@ -4,6 +4,10 @@
 // 1. 모든 인자를 가변인자로 하지 말고
 // 2. 1번째 인자는 독립된 인자(이름있는 변수) 로 받으세요
 
+// 주의 : 아래 코드는 재귀 호출이 아닙니다
+// main => foo(인자3개) => foo(인자2개) => foo(인자한개) => foo() 형태로 호출..
+// => 즉, 모두 다른 함수 호출
+
 void foo() {}
 
 template<typename T, typename ... Types>
@@ -11,9 +15,14 @@ void foo(T value, Types ... args)
 {
 	std::cout << value;
 
-	foo(args...);	// foo(3.4, 'A')
+//	foo(args...);	// foo(3.4, 'A')
 					// foo('A')
 					// foo()		==> 이 경우를 위해서 인자없는 foo 필요
+
+	// C++17 부터는 아래 처럼 하면 "인자 없는 foo()" 는 필요 없습니다
+	// => if constexpr 기술을 사용하려면 C++17 이상..
+	if constexpr (sizeof...(args) > 0)
+		foo(args...);
 }
 
 int main()
