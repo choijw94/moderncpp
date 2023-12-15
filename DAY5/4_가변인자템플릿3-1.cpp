4f452f10052894f77dfeda60db63cf9cb6308732
ecourse <smkang@codenuri.co.kr>
2023-12-15 10:59:11 +0900

    Add files via upload

--- DAY5/4_가변인자템플릿3-1.cpp
+++ DAY5/4_가변인자템플릿3-1.cpp
@@ -7,7 +7,23 @@ void goo(int a, int b, int c) {}
 template<typename ... Types>
 void foo(Types ... args)
 {
-	goo(args);
+	// args : 1, 2, 3
+//	goo(args);	// error. pack 이름 자체를 함수 인자로 사용할수 없습니다.
+				//        pack 안의 요소를 , 로 분리해서 보내야 합니다.
+
+
+	// #1. pack expansion 문법을 알아야 합니다
+	// "pack이름..."           => E1, E2, E3
+	// "pack을사용하는코드..."  => E1을사용하는코드, E2을사용하는코드, E3을사용하는코드
+
+	goo(args...);		// goo(1, 2, 3)
+
+	goo((++args)...);	// goo(++E1, ++E2, ++E3)
+
+	// 아래 2줄의 차이점 생각해 보세요
+	goo(hoo(args...));
+	goo(hoo(args)...);
+
 }
 
 
