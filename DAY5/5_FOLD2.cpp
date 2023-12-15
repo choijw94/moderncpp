03f888fb3d02791792a5a935e01d239be9694f8f
ecourse <smkang@codenuri.co.kr>
2023-12-15 12:01:39 +0900

    Add files via upload

--- DAY5/5_FOLD2.cpp
+++ DAY5/5_FOLD2.cpp
@@ -20,8 +20,17 @@ void Show(Types ... args)
 	// 초기값     : std::cout 
 	// 이항연산자 : <<
 	// => binary left fold
-	(std::cout << ... << args);
+//	(std::cout << ... << args);
 			// ((((std::cout << 1) << 2) << 3) << 4) << 5
+
+
+	// #2. args 위치는 "pack이름" 뿐아니라
+	//     "pack이름을 사용하는 코드" 을 넣어도 됩니다.
+	// 이항연산자 : , 
+	// pack 이름 대신 printv(args) 사용
+	(printv(args), ...);
+	// printv(1), (printv(2), (printv(3), (printv(4), printv(5))))	
+	// => 결국, args 의 모든 요소를 printv 에 전달한것
 
 }
 
