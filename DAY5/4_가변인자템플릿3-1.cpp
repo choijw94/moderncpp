48ab84ecd7e33aad8f4149d6ac021420663e98c5
ecourse <smkang@codenuri.co.kr>
2023-12-15 11:02:38 +0900

    Add files via upload

--- DAY5/4_가변인자템플릿3-1.cpp
+++ DAY5/4_가변인자템플릿3-1.cpp
@@ -13,6 +13,7 @@ void foo(Types ... args)
 
 
 	// #1. pack expansion 문법을 알아야 합니다
+	// => ... 으로 pack 안의 모든 요소를 , 로 분리하는 것
 	// "pack이름..."           => E1, E2, E3
 	// "pack을사용하는코드..."  => E1을사용하는코드, E2을사용하는코드, E3을사용하는코드
 
@@ -21,8 +22,11 @@ void foo(Types ... args)
 	goo((++args)...);	// goo(++E1, ++E2, ++E3)
 
 	// 아래 2줄의 차이점 생각해 보세요
-	goo(hoo(args...));
-	goo(hoo(args)...);
+	goo(hoo(args...));	// goo( hoo(1,2,3)) 의 의미 인데..
+						// hoo 함수는 인자가 한개 이므로 error
+
+	goo(hoo(args)...);	// goo(hoo(1), hoo(2), hoo(3))
+						// 위의 hoo, goo 조건을 만족하므로 ok.. 
 
 }
 
