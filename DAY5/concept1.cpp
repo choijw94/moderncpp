21d0b66b5a5b5e669cac8c75cf14580496ab9a0a
ecourse <smkang@codenuri.co.kr>
2023-12-15 15:35:33 +0900

    Add files via upload

--- DAY5/concept1.cpp
+++ DAY5/concept1.cpp
@@ -10,10 +10,14 @@ template<typename T>
 void foo(T& c)
 {
 	// T 가 포인터 인지 알고 싶다.
+	// => type traits 기술
+	// => 대부분 template partial specialization 기술로 만들게 됩니다.
 	bool b = std::is_pointer_v<T>;
 
+
 	// T 가 size 변경이 가능한 컨테이너인지 알고 싶다.
-	// ?
+	// => template partial specialization 로 만들기는 힘들다.
+	// => 새로운 문법을 도입하자.!! - C++20 concept
 }
 int main()
 {
