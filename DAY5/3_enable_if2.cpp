4664447995996d455e0fbe5c7ebd0da1c9014f89
ecourse <smkang@codenuri.co.kr>
2023-12-15 09:33:52 +0900

    Add files via upload

--- DAY5/3_enable_if2.cpp
+++ DAY5/3_enable_if2.cpp
@@ -8,13 +8,19 @@
 template<typename T> T gcd(T a, T b)
 {
 	// 방법 1. static_assert
+	// => static_assert 는 조건을 만족하지 않으면 에러입니다.
+	// => static_assert 는 조건을 만족하지 에러를 내지 말고, 후보군에서 제외된다면
+	//    어떨까요 ?
 	static_assert(std::is_integral_v<T>, "error not integer");
 
 	return 0;
 }
 
+void gcd(double a, double b) { std::cout << "double\n"; }
+
 int main()
 {
-	gcd(6, 9);
-	gcd(3.1,  5.4);
+	gcd(6, 9);		// T 버전 사용
+	gcd(3.1,  5.4); // double 버전 사용
+	gcd(3.1f, 5.4f); // T 버전 사용, 그런데, 정수타입이 아니므로 static_assert 실패. 에러
 }
