6ed08a5573fd9cd42a809bb2dc4415346bb6ae8b
ecourse <smkang@codenuri.co.kr>
2023-12-15 10:14:32 +0900

    Add files via upload

--- DAY5/3_enable_if6-1.cpp
+++ DAY5/3_enable_if6-1.cpp
@@ -7,9 +7,11 @@ int main()
 	std::string s1;
 
 	// 아래 코드에 대해서 생각해 보세요
-	s1 = 65;
-	std::string s2 = 65;
+	s1 = 65;				// ok. error 아님!
+//	std::string s2 = 65;	// error. 
 
 
 	std::cout << std::is_convertible_v<int, std::string> << std::endl;
+								// false
+								// int 로 string 을 만들수는 없다는 의미
 }
