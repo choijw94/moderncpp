c4332e62457d04a9fab5b5b6873617a2af4992fe
ecourse <smkang@codenuri.co.kr>
2023-12-15 11:55:07 +0900

    Add files via upload

---
+++ DAY5/5_FOLD2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// (args + ...)	    // E1+(E2+(E3+(E4+E5)))		 unary  right fold
+// (args + ... + 0) // E1+(E2+(E3+(E4+(E5+0))))  binary right fold
+
+// (... + args)	    // (((E1+E2)+E3)+E4)+E5		 unary  left fold
+// (0 + ... + args) // ((((0+E1)+E2)+E3)+E4)+E5  binary left fold
+
+
+template<typename T> void printv(const T& a)
+{
+	std::cout << a << std::endl;
+}
+
+template<typename ... Types>
+void Show(Types ... args)
+{
+	// #1. 초기값은 "0" 뿐 아니라 "모든 값(객체)" 를 사용할수 있습니다.
+	//     아래 코드 해석해 보세요
+	(std::cout << ... << args);
+}
+
+int main()
+{
+	Show(1, 2, 3, 4, 5);
+}
+
+
+
