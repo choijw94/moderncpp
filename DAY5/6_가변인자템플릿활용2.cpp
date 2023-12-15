747b585031507dc9231545c39da6349b4e3d2723
ecourse <smkang@codenuri.co.kr>
2023-12-15 13:56:51 +0900

    Add files via upload

--- DAY5/6_가변인자템플릿활용2.cpp
+++ DAY5/6_가변인자템플릿활용2.cpp
@@ -39,11 +39,24 @@ struct argument<0, R(A, Types...)>
 	using type = A;
 };
 
+// N != 0
+template<int N, typename R, typename A, typename ... Types>
+struct argument<N, R(A, Types...)>
+{
+	// int (int, double, char) 의 1번째 인자의 타입(double) 은
+	// int (     double, char) 의 0번째 인자의 타입과 같습니다.
+	
+	// => R(A, Types...)> 의  N 번째 인자의 타입은 
+	// => R(   Types...)> 의  N-1 번째 인자의 타입과 같습니다.
+	using type = typename argument<N - 1, R(Types...)>::type;
+};
+
 
 template<typename T> void foo(T& t)
 {
 	// T : int (int, double, char)
-	typename argument<0, T>::type n; // int
+//	typename argument<0, T>::type n; // int
+	typename argument<2, T>::type n; // char
 
 	std::cout << typeid(n).name() << std::endl; 
 }
