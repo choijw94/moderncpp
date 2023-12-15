68bf1785f8832cb8e4ceda3e98cb1062cbb1ef57
ecourse <smkang@codenuri.co.kr>
2023-12-15 14:56:23 +0900

    Add files via upload

--- DAY5/8_get3.cpp
+++ DAY5/8_get3.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include "tuple.h"
 
+
+
+// primary template 
+template<int N, typename T>
+struct tuple_element
+{
+	using type = T;
+};
+
+
 template<typename TP>
 void foo(TP& tp)
 {
+	
 	// TP : tuple<int, double, short>
 	typename tuple_element<1, TP>::type n; // double
 
+
+
 	std::cout << typeid(n).name() << std::endl;
 }
+
+
+
+
 
 int main()
 {
